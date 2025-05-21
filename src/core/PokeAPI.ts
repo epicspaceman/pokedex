9ae74230abd55821c614b6f8eb69819baf81876d
Robert Lockay <robert.lockay@gmail.com>
2025-05-20 21:02:35 -0500

    abstracted generation of random pokemon id

--- src/core/PokeAPI.ts
+++ src/core/PokeAPI.ts
@@ -1,7 +1,5 @@
 import { EvolutionChain, Pokemon, PokemonSpecies } from "../lib/defintions"
-
-const MAX_POKEMON_ID = 1000;
-const MIN_POKEMON_ID = 1;
+import generateRandomPokemonId from "./RandomPokemonId"
 
 export async function fetchData<t>(url: string): Promise<t | Response>  {
     const response = await fetch(url, {method: 'GET'})
@@ -17,7 +15,7 @@ export async function fetchData<t>(url: string): Promise<t | Response>  {
 
 export async function fetchPokemon(identifier?: number | string): Promise<Pokemon | Response> {
     // when given no identifier, generate a random id between the maximum and minimum pokemon ids
-    const id = identifier ? identifier : Math.floor(Math.random() * (MAX_POKEMON_ID-MIN_POKEMON_ID) + MIN_POKEMON_ID)
+    const id = identifier ? identifier : generateRandomPokemonId()
     const url = `https://pokeapi.co/api/v2/pokemon/${id}/`
 
     return fetchData<Pokemon>(url)
